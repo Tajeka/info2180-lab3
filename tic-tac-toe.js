247cffb56d6f01c9ef911952a3081c8596dac022
Tajeka <johnsontajeka@gmail.com>
2023-10-18 16:10:29 -0500

    Winner being displayed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -39,6 +39,48 @@ window.addEventListener("load", (event) => {
     elem.addEventListener("mouseout", function (e) {
       e.target.classList.remove("hover");
     });
-  });
+  }); 
 
+  const winner_combination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  function winner() {
+    let x = "";
+    let y = "";
+    let z = "";
+
+    winner_combination.forEach((c) => {
+      if (sq[c[0]].hasChildNodes()) {
+        x = sq[c[0]].childNodes[0].textContent;
+      } else {
+        x = "";
+      }
+      if (sq[c[1]].hasChildNodes()) {
+        y = sq[c[1]].childNodes[0].textContent;
+      } else {
+        y = "";
+      }
+      if (sq[c[2]].hasChildNodes()) {
+        z = sq[c[2]].childNodes[0].textContent;
+      } else {
+        z = "";
+      }
+      if (z !== "" && y !== "" && x !== "") {
+        if (x == y && x == z) {
+          // Winner
+          let w = document.getElementById("status");
+          w.textContent = `Congratulations! ${x} is the Winner!`;
+          w.classList.add("you-won");
+        }
+      }
+    });
+  }
 });
