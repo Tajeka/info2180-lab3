ac7e8e321fc7f4e2feeeb2415b37514ddb99a3d7
Tajeka <johnsontajeka@gmail.com>
2023-10-18 16:00:31 -0500

    Display X and O on square

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -12,5 +12,24 @@ window.addEventListener("load", (event) => {
     for (let i = 0; i < div.length; i++) {
       div[i].className = "square";
     }
-  }
+  } 
+
+  const sq = document.querySelectorAll(".square");
+
+  let c = 0;
+  for (let i = 0; i < sq.length; i++) {
+    sq[i].addEventListener("click", (e) => {
+      if (c % 2 == 0) {
+        e.target.classList.add("X");
+        sq[i].textContent = "X";
+        c++;
+      } else {
+        e.target.classList.add("O");
+        sq[i].textContent = "O";
+        c++;
+      }
+    });
+  };
+
+
 });
