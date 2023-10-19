b3b05bdbe7ec4f230faa0d31d9516118ca480a6d
Tajeka <johnsontajeka@gmail.com>
2023-10-18 20:45:54 -0500

    Elements cannot change after set(Disallow cheating). After winner, clicking disallowed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,6 @@
 const game = document.querySelector("#game");
 
 window.addEventListener("load", (event) => {
-  // Insert code here
-
   let board = document.getElementById("board");
 
   let div = board.childNodes;
@@ -12,24 +10,37 @@ window.addEventListener("load", (event) => {
     for (let i = 0; i < div.length; i++) {
       div[i].className = "square";
     }
-  } 
+  }
 
   const sq = document.querySelectorAll(".square");
+
+  let winner_status = document.getElementById("status");
 
   let c = 0;
   for (let i = 0; i < sq.length; i++) {
     sq[i].addEventListener("click", (e) => {
-      if (c % 2 == 0) {
-        e.target.classList.add("X");
-        sq[i].textContent = "X";
-        c++;
-      } else {
-        e.target.classList.add("O");
-        sq[i].textContent = "O";
-        c++;
+      if (
+        winner_status.textContent != `Congratulations! O is the Winner!` &&
+        winner_status.textContent != `Congratulations! X is the Winner!`
+      ) {
+        if (c % 2 == 0) {
+          if (e.target.textContent == "") {
+            e.target.classList.add("X");
+            sq[i].textContent = "X";
+            c++;
+            winner();
+          }
+        } else {
+          if (e.target.textContent == "") {
+            e.target.classList.add("O");
+            sq[i].textContent = "O";
+            c++;
+            winner();
+          }
+        }
       }
     });
-  };
+  }
 
   sq.forEach(function (elem) {
     elem.addEventListener("mouseover", function (e) {
@@ -39,7 +50,7 @@ window.addEventListener("load", (event) => {
     elem.addEventListener("mouseout", function (e) {
       e.target.classList.remove("hover");
     });
-  }); 
+  });
 
   const winner_combination = [
     [0, 1, 2],
@@ -84,8 +95,8 @@ window.addEventListener("load", (event) => {
     });
   }
 
+  //
   const btn = document.querySelector(".btn");
-  console.log(btn);
   btn.addEventListener("click", function reset() {
     document.getElementById("status").textContent =
       "Move your mouse over a square and click to play an X or an O.";
@@ -96,6 +107,4 @@ window.addEventListener("load", (event) => {
       e.textContent = "";
     });
   });
-
-
 });
